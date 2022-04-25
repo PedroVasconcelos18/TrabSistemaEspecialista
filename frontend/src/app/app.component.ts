f3d72baef1480c04445c49adb3d848193d8bcf70
pedrosales <pedrosales@frwk.com.br>
2022-04-24 21:55:47 -0300

    Commit inicial

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TrabSistemaEspecialista';
+
+  constructor(private toastr: ToastrService) {}
+
+  showSuccess() {
+    this.toastr.success("Criado com sucesso")
+  }
+
+
+
+}
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
